da136a49e5568560378b782e02b8099dee278d09
zFunx <20762882+zFunx@users.noreply.github.com>
2023-10-30 20:38:42 +0530

    No items found component

---
+++ src/components/NoItemsFound.js
@@ -0,0 +1,11 @@
+const NoItemsFound = () => {
+  return (
+    <div className="flex justify-center items-center h-64">
+      <div className="text-center">
+        <p className="text-gray-600 text-3xl font-bold">No items found</p>
+      </div>
+    </div>
+  );
+};
+
+export default NoItemsFound;
--- src/components/WarehousePane.js
+++ src/components/WarehousePane.js
@@ -1,23 +1,28 @@
 import WarehouseCard from "./WarehouseCard";
 import WarehouseImage from "@/assets/images/Warehouse.jpg";
 import { useSelector, useDispatch } from "react-redux";
+import NoItemsFound from "./NoItemsFound";
 
 const WarehousePane = () => {
   const filteredWarehouses = useSelector(
     (state) => state.warehouses.filteredWarehouses
   );
 
-  return (
-    <div className="flex gap-4 flex-wrap justify-center">
-      {filteredWarehouses.map((warehouse) => (
-        <WarehouseCard
-          key={warehouse.id}
-          {...warehouse}
-          imageSrc={WarehouseImage.src}
-        />
-      ))}
-    </div>
-  );
+  if (filteredWarehouses.length > 0) {
+    return (
+      <div className="flex gap-4 flex-wrap justify-center">
+        {filteredWarehouses.map((warehouse) => (
+          <WarehouseCard
+            key={warehouse.id}
+            {...warehouse}
+            imageSrc={WarehouseImage.src}
+          />
+        ))}
+      </div>
+    );
+  } else {
+    return <NoItemsFound />;
+  }
 };
 
 export default WarehousePane;
